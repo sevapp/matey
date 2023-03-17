6bbe8205ad548ab70cc420656e942be6911d8f4d
Artem <artp80@gmail.com>
2023-03-17 18:54:51 +0600

    test: test view of UL

---
+++ examples/draft.ts
@@ -0,0 +1,25 @@
+// const cli = new Cli();
+// cli
+//   .addCommand(cmdA)
+//   .addCommand(cmdB)
+//   .addCommand(cmdC)
+//   .use(rexExpA, handlerA)
+//   .use(rexExpB, handlerB);
+
+// const cmdA = new CliCommandBuilder()
+//   .setName('cmdA')
+//   .setDescription('cmdA description')
+//   .addArgument(argA_1)
+//   .addArgument(argA_2)
+//   .addSubcommand(subA)
+//   .setHandler(handlerA)
+//   .build();
+
+// const argA_1 = {
+//   name: '--argA_1',
+//   description: 'argA_1 description',
+//   type: ArgumentType.OPTION,
+//   valueValidator: val:string => val.length > 0,
+//   optionNameRequired: true,
+//   required: false,
+// };
